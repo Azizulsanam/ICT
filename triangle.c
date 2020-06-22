831369c2df8a5a9f32c48609629a57b8c0fcc284
Azizulsanam <54774857+Azizulsanam@users.noreply.github.com>
2020-06-22 23:35:50 +0600

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  // header file
+#include <math.h>   // header file
+int main()
+{
+    float a,b,c,s,area; // variable
+
+    scanf("%f%f%f",&a,&b,&c);// input
+
+    s=(a+b+c)/2; // half perimeter
+
+    if( a+b>c && b+c>a && a+c>b) // condition
+    {
+        area= sqrt(s*(s-a)*(s-b)*(s-c)); // formula
+
+        printf("The area is =%f",area)  // output
+    }
+    else
+    {
+        printf("The triangle is impossible"); //output
+    }
+
+
+    return 0;
+}
